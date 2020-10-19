c26f9550ad2dec1b8a6c4b5d3a82b8fefd8c62a4
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-19 13:42:47 -0500

    [Feture] Mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+    Mixed lists
+"""
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """
+        Args:
+            mxd_lst: float-int numbers
+
+        Return:
+            Float base in int or float numbers
+    """
+
+    result: float = 0
+
+    for x in input_list:
+        result += x
+
+    return result
