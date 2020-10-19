8c787b1b0a1de5f62ea90b8755d8584076bed24f
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-19 22:07:39 +0000

    [Fix] Change the base code :/

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,10 +2,10 @@
 """
     Type checking
 """
-from typing import Tuple
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
         Args:
             lst: Tuple of numbers
@@ -14,15 +14,15 @@ def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
         Return:
             zoomed
     """
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
