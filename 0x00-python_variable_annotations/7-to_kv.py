88d960503809cf28af1a316c29db0cb43f7dda91
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-19 14:26:51 -0500

    [Feture] Typing Optional

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from typing import Union, List, Tuple, Optional
+"""
+    Mixed lists
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """
+        Args:
+            k: String
+            v: Union: Can be int or float
+
+        Return:
+            Tuple with string and int or float
+    """
+
+    cncat: Tuple(str, Optional[int, float])
+
+    return cncat
