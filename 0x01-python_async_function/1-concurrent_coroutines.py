c0ee3a985e2d0c50cebd4e486fdedadab6d987f4
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-20 12:22:42 -0500

    [Feature] Concurrent coroutine

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Coroutine at the same time witha sync """
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int = 10, n: int = 0) -> List[float]:
+    """
+        Args:
+            max_delay: max wait
+            n: spawn function
+
+        Return:
+            float time random
+    """
+    delays: List[float] = []
+    tasks: List = []
+
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    for task in asyncio.as_completed((tasks)):
+        delay = await task
+        delays.append(delay)
+
+    return delays
