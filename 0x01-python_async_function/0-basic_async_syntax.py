ca52aa8c9e9e23f60cbf02a0a6e05adf5229e1c1
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-20 10:47:39 -0500

    [Feature] Add the time of wait

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,6 +12,7 @@ async def wait_random(max_delay: int = 10) -> float:
         Return:
             float time random
     """
-    i: float = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
 
-    return i
+    return delay
