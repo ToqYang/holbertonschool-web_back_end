44ad07c78ad98f791fbde508cb31c3b08419ec20
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-20 10:20:12 -0500

    [Feature] SyncIo intro

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Basic syntax await async """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+        Args:
+            max_delay: max wait
+
+        Return:
+            float time random
+    """
+    i: float = random.uniform(0, max_delay)
+
+    return i
