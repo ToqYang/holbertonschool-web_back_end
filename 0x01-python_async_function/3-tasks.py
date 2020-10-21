1888f29df2b5bd2c49db49bf7c6fe1b6cf1a3a43
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-20 19:40:08 -0500

    [Fix] Add typing

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int = 10):
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """
         Args:
             max_delay: max wait
