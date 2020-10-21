20c03b5282d94349abacabbd147f6bfc95a446ef
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-21 11:47:45 -0500

    [Feature] Put return typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Async Generator """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
         Generate numbers
 
