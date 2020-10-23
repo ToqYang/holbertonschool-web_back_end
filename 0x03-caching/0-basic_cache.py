a0f29f86dbfad2af80cac9d2beaa5a9ca432a55e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-10-23 11:28:40 -0500

    [Feature] Basic cache

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    BaseCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+                item: value of the key
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
