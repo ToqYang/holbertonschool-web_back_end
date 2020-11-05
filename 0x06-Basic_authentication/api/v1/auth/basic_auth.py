9e49e45ca26538da61ad9ba4c7028c011cbd729a
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-05 14:59:05 -0500

    [Feature] base64 auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,5 +5,28 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ Bacis Auth class """
-    pass
+    """ Basic Auth class """
+    def __init__(self):
+        """Constructor"""
+
+    def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str
+                                            ) -> str:
+        """
+            Extract header in base64
+
+            Args:
+                authorization_header: string in base64
+
+            Return:
+                Header in base64
+        """
+        if authorization_header is None\
+           or type(authorization_header) != str\
+           or not authorization_header.startswith('Basic ')\
+           and not authorization_header.endswith(' '):
+
+            return None
+
+        return authorization_header.split(' ')[1]
