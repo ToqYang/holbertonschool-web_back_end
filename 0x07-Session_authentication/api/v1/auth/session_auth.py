fae97efa63f4e977b8fac5a2ff381c326ef4582a
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-14 16:31:19 -0500

    [Feature] Make new sessions

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,28 @@
 """ Module of Session Auth
 """
 from api.v1.auth.auth import Auth
+from typing import Dict
+from uuid import uuid4, UUID
 
 
 class SessionAuth(Auth):
     """ Auth Class """
-    pass
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            Make a new Session and register in the class
+
+            Args:
+                user_id: Identificator of the user_id
+
+            Return:
+                Session ID
+        """
+        if user_id is None and isinstance(user_id, str) is False:
+            return None
+
+        session_id: UUID = uuid4()
+        self.user_id_by_session_id['user_id'] = session_id
+
+        return session_id
