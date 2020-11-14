4ac40722dedf65ace90fc3e89fde6c8194412e4b
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-14 17:06:03 -0500

    [Fix] Change type to compare

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,20 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+            Make a user ID based in session id
+
+            Args:
+                session_id: String of the session
+
+            Return:
+                User ID
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+
+        user_id: str = self.user_id_by_session_id.get(session_id)
+
+        return user_id
