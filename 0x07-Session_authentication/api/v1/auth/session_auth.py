3f0fc2e61bb89b246f136a54b46de7d6cb8b8056
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-14 16:40:16 -0500

    [Fix] keys and values of the dict sessions

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Module of Session Auth
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 from typing import Dict
 from uuid import uuid4, UUID
 
@@ -23,7 +24,7 @@ class SessionAuth(Auth):
         if user_id is None and isinstance(user_id, str) is False:
             return None
 
-        session_id: UUID = uuid4()
-        self.user_id_by_session_id['user_id'] = session_id
+        session_id: str = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
 
         return session_id
