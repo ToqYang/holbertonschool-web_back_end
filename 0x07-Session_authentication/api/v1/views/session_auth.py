5d55f5bb830bdba182c40ca1b69e5a45c6b1fcea
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-14 22:00:09 -0500

    [Fix] Get the enviroment

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -27,22 +27,18 @@ def login():
     if not passwd:
         return make_response(jsonify({"error": "password missing"}), 400)
 
-    exist_user: TypeVar('User')
+    exist_user = User.search({"email": email})
 
-    try:
-        exist_user = User.search({"email": email})
-    except Exception:
-        return jsonify({"error": "no user found for this email"}), 404
-
-    if not exist_user:
+    if len(exist_user) == 0:
         return jsonify({"error": "no user found for this email"}), 404
 
     from api.v1.app import auth
     for user in exist_user:
         if (user.is_valid_password(passwd)):
             session_id = auth.create_session(user.id)
+            SESSION_NAME = getenv('SESSION_NAME')
             response = make_response(user.to_json())
-            response.set_cookie('SESSION_NAME', session_id)
+            response.set_cookie(SESSION_NAME, session_id)
             return response
 
     return make_response(jsonify({"error": "wrong password"}), 401)
