36531cb8c268231a4170df5bfd0be6eab384c98e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-22 14:05:08 -0500

    [Featue] Register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 """ Flask module """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def hello_world() -> str:
     """ Greetings """
     msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """ Make a new user """
+    try:
+        email = request.form['email']
+        pwd = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, pwd)
+    except ValueError:
+        msg = {"message": "email already registered"}
+        return jsonify(msg), 400
+
+    msg = {"email": user.email, "message": "user created"}
+
     return jsonify(msg)
 
 
