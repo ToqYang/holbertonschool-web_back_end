1fea89cddbeaf85fd9470ef6c3f50876c71fd8fe
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-23 18:47:42 -0500

    [Fix] Change the word of the code

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -177,12 +177,13 @@ class Auth:
 
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            new_passwd = _hash_password(password)
-
-            self._db.update_user(
-                (user.id), {"hashed_password": new_passwd,
-                            "reset_token": None})
-
-            return None
         except (NoResultFound, InvalidRequestError):
             raise ValueError
+
+        new_passwd = _hash_password(password)
+
+        self._db.update_user(
+            (user.id), {"hashed_password": new_passwd,
+                        "reset_token": None})
+
+        return None
