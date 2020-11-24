00022176a56f6db6f4395fcb9234b98e81072c30
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-24 17:03:16 -0500

    [Feature] Test get json

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,9 @@
 """ Test utils
 """
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch
+import requests
+from utils import access_nested_map, get_json
 from parameterized import parameterized, parameterized_class
 
 
@@ -15,7 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path_map, result_expec):
-        """ Access nested method """
+        """ Access nested method
+
+            args:
+                nested_map: {"a": 1},
+                path: ("a",)
+                result_expec: 1
+
+            return
+                Ok if its correct
+        """
         self.assertEqual(access_nested_map(nested_map, path_map), result_expec)
 
     @parameterized.expand([
@@ -23,9 +34,39 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path_map):
-        """ Exception access nested method """
+        """ Exception access nested method
+
+            args:
+                nested_map: {}
+                path: ("a",)
+
+            return:
+                ok if its correct
+        """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path_map)
 
         self.assertEqual(
             f'KeyError({str(error.exception)})', repr(error.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Test JSON """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Mock HTTP calls
+
+            args:
+                url: Web page to look
+                response: result of the consult
+        """
+        conf = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **conf)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
