e2eed2ccb555b3d021078cd78f1beaf02648d53d
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-28 17:08:30 -0500

    [Fix] Add Put the index html

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -5,6 +5,7 @@ from flask_babel import Babel
 
 
 class Config(object):
+    """ Configuration Babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
@@ -15,14 +16,14 @@ app.config.from_pyfile(Config)
 babel = Babel(app)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
     """ Greeting
 
         Return:
             Initial template html
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
