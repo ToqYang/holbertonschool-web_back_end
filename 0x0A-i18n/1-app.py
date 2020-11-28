9e52ce5218926aa8c4a10b7e5cbdcb13d4f09467
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-28 17:04:02 -0500

    [Fix] Add features

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
+""" Basic Babel setup """
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+
+
 app = Flask(__name__)
+app.config.from_pyfile(Config)
+babel = Babel(app)
 
 
 @app.route('/')
