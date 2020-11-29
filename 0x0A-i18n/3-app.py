91f76b5748b8571fd86fa42e8b0b25e200d3abdf
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-28 20:09:54 -0500

    [Feature] Put method

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -26,7 +26,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', method=['GET'], strict_slashes=False)
 def hello_world():
     """ Greeting
 
