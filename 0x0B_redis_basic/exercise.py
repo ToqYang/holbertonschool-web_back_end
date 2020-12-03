f922b51f1113860cda64748b5d9984d29824039a
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-02 21:49:32 -0500

    [Feature] Reading redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
     String Redis
 """
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 import redis
 
 
@@ -29,3 +29,35 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
+        """
+            Store the cache
+
+            Args:
+                data: bring the information to store
+
+            Return:
+                Key or number uuid
+        """
+        key = self._redit.get(key)
+
+        if fn:
+            return fn(key)
+
+        return key
+
+    def get_str(self, key: str) -> str:
+        """ Parametrized get str """
+        return self._redit.get(key).decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Parametrized get int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        exception Exception:
+            value = 0
+
+        return value
