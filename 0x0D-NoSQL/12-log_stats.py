a5bd1b0387624a0f161dac4976259a006746a6c5
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-10 18:26:55 -0800

    [Feature] Search log stats

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Log stats """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db_nginx = client.logs.nginx
+    methods = {"GET", "POST", "PUT", "PATCH", "DELETE"}
+
+    print('Methods:')
+    for method in methods:
+        count_method = db_nginx.count_documents({'method': method})
+        print(f'\tmethod: {count_method}')
+
+    check = db_nginx.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{check} status check')
