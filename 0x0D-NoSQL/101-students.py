7901a9f3a93227dc0844e0c091fac85d292ac0f2
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-11 13:13:37 -0800

    [Feature] Add file with solution but with error indentation

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Top students  """
+
+
+def top_students(mongo_collection):
+    """ Average top students
+
+        Args:
+            mongo_collection: Collection to find avg top
+
+        Return:
+            List all top students
+    """
+    top_students = mongo_collection.aggregate{[
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ]}
+
+    return top_students
