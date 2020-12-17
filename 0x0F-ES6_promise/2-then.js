b85fadd5bdd72c2881df1ca727d5699744a22cce
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-17 13:28:59 -0500

    [Fix] eslint

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -3,15 +3,13 @@
     because independent this pass a promise
 */
 function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            return {
-                status: 200,
-                body: 'Success',
-            }
-        })
-        .catch(() => new Error())
-        .finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
