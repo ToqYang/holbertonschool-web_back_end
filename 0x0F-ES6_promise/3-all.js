66ca21d602ad2a5bc15bdb68643b868ae9312dc4
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-17 14:15:38 -0500

    [Fix] Change log error to log wtf

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -12,7 +12,7 @@ function handleProfileSignup() {
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
 
