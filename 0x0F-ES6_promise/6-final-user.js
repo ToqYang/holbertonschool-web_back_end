d7ee4178330ff997b0fe73a90cb1dc34d4c1dd88
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-17 19:57:03 -0500

    [Feature] Simple settled

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,10 +1,9 @@
-//
+// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((promiseRes) => console.log(promiseRes));
 }
 
 export default handleProfileSignup;
