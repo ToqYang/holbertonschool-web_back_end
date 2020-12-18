effaf02fa0f449e2df9e9dcc95f56a9ab7795d57
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-17 19:58:57 -0500

    [Fix] eslint

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
 
 export default handleProfileSignup;
