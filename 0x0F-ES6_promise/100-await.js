d92def396b93e2f8f94c9045e2915e9c961f8a1e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-17 22:18:41 -0500

    [Fix] eslint

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -2,17 +2,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 const asyncUploadUser = async () => {
-    let upload;
-    let make;
+  let upload;
+  let make;
 
-    try {
-        upload = await uploadPhoto();
-        make = await createUser();
-
-    } catch(e) {
-        upload = null;
-        make = null;
-    }
+  try {
+    upload = await uploadPhoto();
+    make = await createUser();
+  } catch (e) {
+    upload = null;
+    make = null;
+  }
   return ({ upload, make });
 };
 
