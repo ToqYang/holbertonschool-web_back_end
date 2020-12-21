afcd38a2a216996f996db4a1eaaf36c02ced23a0
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-21 16:15:03 -0500

    [Fix] Error message

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -21,7 +21,7 @@ export default class Airport {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw new TypeError('name must be a string');
     }
     this._name = value;
   }
@@ -32,7 +32,7 @@ export default class Airport {
 
   set code(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw new TypeError('code must be a string');
     }
     this._code = value;
   }
