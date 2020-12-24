04f9198001c9fa19cfbb83c804dbc9ad1805b930
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-23 22:22:20 -0500

    [Feature] Reduce functionality

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,24 +1,24 @@
-//
+// Grade by city and add grade
 
 const updateStudentGradeByCity = (students, city, newGrades) => {
   if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
 
-  const studentByCity = students.filter((student) => student.location === city);
+  const studentByCity = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grades = newGrades.filter((note) => student.id === note.studentId);
+      let grade = 'N/A';
 
-  const studentByGrade = studentByCity.map((student) => {
-    const grades = newGrades.filter((note) => student.id === note.studentId);
-    let grade = 'N/A';
+      if (grades[0]) {
+        grade = grades[0].grade;
+      }
 
-    if (grades[0]) {
-      grade = grades[0].grade;
-    }
+      return { ...student, grade };
+    });
 
-    return { ...student, grade };
-  });
-
-  return studentByGrade;
+  return studentByCity;
 };
 
 export default updateStudentGradeByCity;
