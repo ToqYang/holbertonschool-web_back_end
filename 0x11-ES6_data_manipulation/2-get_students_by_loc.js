f62c6d36c7a6caf496be307122c719f11cc5da22
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-23 21:10:31 -0500

    [Fix] Eslint

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,7 +2,7 @@
 
 const getStudentsByLocation = (students, city) => {
   const studentsLocation = students.filter(
-    (student) => student.location === city
+    (student) => student.location === city,
   );
 
   return studentsLocation;
