8f47fbbc460d4740b2cee3dadfc035c7dd33645c
Santiago Yanguas <santitoya-2001@outlook.com>
2020-12-25 16:49:11 -0500

    [Fix] eslint

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -2,11 +2,11 @@
 
 const groceriesList = () => {
   const fruits = new Map([
-    ["Apples", 10],
-    ["Tomatoes", 10],
-    ["Pasta", 1],
-    ["Rice", 1],
-    ["Banana", 5],
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
   ]);
 
   return fruits;
